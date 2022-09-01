36e1e86f9cad9105dfa3305622cfef83d7253340
masachi <zhangjb42@gmail.com>
2022-09-01 13:37:06 +0800

    fix

--- index.js
+++ index.js
@@ -7,25 +7,12 @@ const APP_PORT = 3000
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const CURRENT_HOST = "https://telegram-bot-omega-nine.vercel.app/"
 
-const bot = new Telegraf(token, {
-  telegram: {
-    webhookReply: false
-  }
-})
-
-bot.on('text', ctx => {
-  return ctx.reply(
-    `msg recebida de: ${ctx.message.from.username}`
-  )
-})
-
-app.use(bot.webhookCallback('/callback'))
-
-app.get('/', async (_req, res) => {
-  const url = `${CURRENT_HOST}/callback`
-  await bot.telegram.setWebhook(url)
-  res.send(`listening on ${CURRENT_HOST}`)
-})
+const bot = new Telegraf(token);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
 
 app.listen(APP_PORT, () => {
   console.log(`listening on ${APP_PORT}`)
