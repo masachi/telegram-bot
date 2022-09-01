e4f53a20e18c9388e660ea1594c8244a7ae1106e
masachi <zhangjb42@gmail.com>
2022-09-01 13:29:10 +0800

    fix

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const TelegramBot = require('node-telegram-bot-api');
+
+const app = express()
+
+const APP_PORT = 3000
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const CURRENT_HOST = 'https://telegram-bot-omega-nine.vercel.app'
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('text', ctx => {
+  return ctx.reply(
+    `msg recebida de: ${ctx.message.from.username}`
+  )
+})
+
+app.use(bot.webhookCallback('/callback'))
+
+app.get('/', async (_req, res) => {
+  const url = `${CURRENT_HOST}/callback`
+  await bot.telegram.setWebhook(url)
+  res.send(`listening on ${CURRENT_HOST}`)
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`listening on ${APP_PORT}`)
+})
