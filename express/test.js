c9aebc614dbee050e54c324422f1c08e2a5c456c
masachi <zhangjb42@gmail.com>
2022-09-08 13:29:17 +0800

    test

--- express/test.js
+++ express/test.js
@@ -1,18 +1,23 @@
 const { Telegraf } = require('telegraf')
-const express = require('express')
-const APP_PORT = 3000;
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const Koa = require('koa')
+const koaBody = require('koa-body')
 
-const bot = new Telegraf(token);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+bot.telegram.setWebhook('https://unusual-bedclothes-crow.cyclic.app/YukinoshitaKyaru')
 
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
-// Set telegram webhook
-bot.telegram.setWebhook('https://unusual-bedclothes-crow.cyclic.app/YukinoshitaKyaru')
-
-const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.use(bot.webhookCallback('/YukinoshitaKyaru'))
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+const app = new Koa()
+app.use(koaBody())
+app.use(async (ctx, next) => {
+  if (ctx.method !== 'POST' || ctx.url !== '/YukinoshitaKyaru') {
+    return next()
+  }
+  await bot.handleUpdate(ctx.request.body, ctx.response)
+  ctx.status = 200
 })
+app.use(async (ctx) => {
+  ctx.body = 'Hello World'
+})
+
+app.listen(3000)
