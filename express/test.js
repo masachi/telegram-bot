9047505b4fb5208c37bc4049ce9a8b1117740bed
masachi <zhangjb42@gmail.com>
2022-09-08 13:00:43 +0800

    test

--- express/test.js
+++ express/test.js
@@ -1,25 +1,18 @@
-const { Telegraf } = require("telegraf");
-const Koa = require("koa")
-const koaBody = require("koa-body");
-
+const Telegraf = require('telegraf')
+const express = require('express')
 const APP_PORT = 3000;
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new Telegraf(token);
-const app = new Koa();
 
-// First reply will be served via webhook response,
-// but messages order not guaranteed due to `koa` pipeline design.
-// Details: https://github.com/telegraf/telegraf/issues/294
-bot.command("image", ctx =>
-	ctx.replyWithPhoto({ url: "https://picsum.photos/200/300/?random" }),
-);
-bot.on("text", ctx => ctx.reply("Hello"));
+bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
-app.use(koaBody());
+// Set telegram webhook
+bot.telegram.setWebhook('https://unusual-bedclothes-crow.cyclic.app/YukinoshitaKyaru')
 
-app.use(async (ctx, next) =>
-	(await bot.createWebhook({ domain: "https://unusual-bedclothes-crow.cyclic.app" }))(ctx.req, ctx.res, next),
-);
-
-app.listen(APP_PORT, () => console.log("Listening on port", APP_PORT));
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bot.webhookCallback('/YukinoshitaKyaru'))
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
