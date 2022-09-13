9ec52f60bf8c47ad1afc6c813ce1952af0975c54
masachi <zhangjb42@gmail.com>
2022-09-13 10:18:50 +0800

    fix: undefined

--- service/photo.js
+++ service/photo.js
@@ -5,24 +5,24 @@ const processPhotoMessage = async (ctx, message) => {
   console.log("Photo Message", JSON.stringify(message));
   if (message.photo && message.photo.length > 0) {
     let uploadContent = {};
-    uploadContent[message.photo[0].file_unique_id] = [];
+    uploadContent[message.photo[0].file_id] = [];
     for(let photoItem of message.photo) {
       let uploadResponse = await uploadPhotoToTelegraph(ctx, photoItem);
-      if(uploadContent[photoItem.file_unique_id]) {
-        uploadContent[photoItem.file_unique_id].push({
+      if(uploadContent[photoItem.file_id]) {
+        uploadContent[photoItem.file_id].push({
           ...photoItem,
           ...uploadResponse
         }) 
       }
       else {
-        uploadContent[photoItem.file_unique_id] = [{
+        uploadContent[photoItem.file_id] = [{
           ...photoItem,
           ...uploadResponse
         }]
       }
     }
     await appendGistByGistId(process.env.GIST_ID, uploadContent, process.env.FILE_NAME);
-    return uploadContent[message.photo[0].file_unique_id][uploadContent.length - 1];
+    return uploadContent[message.photo[0].file_id][uploadContent.length - 1];
   }
 
   return {
