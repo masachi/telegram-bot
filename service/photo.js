811c660568acc2294d3b57739a6eae2d10efc9b0
masachi <zhangjb42@gmail.com>
2022-09-13 10:25:06 +0800

    revert

--- service/photo.js
+++ service/photo.js
@@ -4,25 +4,16 @@ const {appendGistByGistId} = require("../service/gist");
 const processPhotoMessage = async (ctx, message) => {
   console.log("Photo Message", JSON.stringify(message));
   if (message.photo && message.photo.length > 0) {
-    let uploadContent = {};
-    uploadContent[message.photo[0].file_id] = [];
+    let uploadContent = [];
     for(let photoItem of message.photo) {
       let uploadResponse = await uploadPhotoToTelegraph(ctx, photoItem);
-      if(uploadContent[photoItem.file_id]) {
-        uploadContent[photoItem.file_id].push({
-          ...photoItem,
-          ...uploadResponse
-        }) 
-      }
-      else {
-        uploadContent[photoItem.file_id] = [{
-          ...photoItem,
-          ...uploadResponse
-        }]
-      }
+      uploadContent.push({
+        ...photoItem,
+        ...uploadResponse
+      })
     }
     await appendGistByGistId(process.env.GIST_ID, uploadContent, process.env.FILE_NAME);
-    return uploadContent[message.photo[0].file_id][uploadContent.length - 1];
+    return uploadContent[uploadContent.length - 1];
   }
 
   return {
