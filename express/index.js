5a24f960e1a23fad3053f1c24eeeeec5de63a61c
masachi <zhangjb42@gmail.com>
2022-09-30 11:31:39 +0800

    fix: ua header

--- express/index.js
+++ express/index.js
@@ -15,19 +15,55 @@ bot.telegram.setWebhook(webhookUrl);
 bot.start((ctx) => ctx.reply("用法问问dalao？"));
 bot.help((ctx) => ctx.reply("没有帮助，问dalao去.jpg"));
 
+// {
+//   twitterSite: '@github',
+//   twitterCard: 'summary_large_image',
+//   twitterTitle: 'GitHub - rocksdanister/lively: Free and open-source software that allows users to set animated desktop wallpapers and screensavers.',
+//   twitterDescription: 'Free and open-source software that allows users to set animated desktop wallpapers and screensavers. - GitHub - rocksdanister/lively: Free and open-source software that allows users to set animated...',
+//   ogSiteName: 'GitHub',
+//   ogType: 'object',
+//   ogTitle: 'GitHub - rocksdanister/lively: Free and open-source software that allows users to set animated desktop wallpapers and screensavers.',
+//   ogUrl: 'https://github.com/rocksdanister/lively',
+//   ogDescription: 'Free and open-source software that allows users to set animated desktop wallpapers and screensavers. - GitHub - rocksdanister/lively: Free and open-source software that allows users to set animated...',
+//   ogImage: {
+//     url: 'https://repository-images.githubusercontent.com/201188122/a89aff42-e775-4da1-b487-cee679c7c8a6',
+//     width: null,
+//     height: null,
+//     type: null
+//   },
+//   twitterImage: {
+//     url: 'https://repository-images.githubusercontent.com/201188122/a89aff42-e775-4da1-b487-cee679c7c8a6',
+//     width: null,
+//     height: null,
+//     alt: null
+//   },
+//   ogLocale: 'en',
+//   favicon: 'https://github.githubassets.com/favicons/favicon.svg',
+//   charset: 'utf8',
+//   requestUrl: 'https://github.com/rocksdanister/lively',
+//   success: true
+// }
+
 bot.on("text", async (ctx) => {
   const message = ctx.message;
-  console.log(
-    "message info",
-    message
-  );
+  console.log("message info", message);
   if (message.entities && message.entities.length > 0) {
     if (message.entities[0].type === "url") {
-      let data = await ogs({ url: message.text });
+      let data = await ogs({
+        url: message.text,
+        headers: {
+          "user-agent": "Googlebot/2.1 (+http://www.google.com/bot.html)",
+        },
+      });
       const { error, result, response } = data;
       console.log("error:", error); // This returns true or false. True if there was an error. The error itself is inside the results object.
       console.log("result:", result); // This contains all of the Open Graph results
-      console.log("response:", response); // This contains the HTML of page
+
+      if(error) {
+        ctx.reply("好像失败了~");
+        return;
+      }
+
       ctx.reply("link yes!");
       return;
     }
