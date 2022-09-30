89f7e0878c93e62b5041be33f3dd8e5de6f87020
masachi <zhangjb42@gmail.com>
2022-09-30 11:15:23 +0800

    fix: url

--- express/index.js
+++ express/index.js
@@ -18,18 +18,17 @@ bot.help((ctx) => ctx.reply("没有帮助，问dalao去.jpg"));
 bot.on("text", (ctx) => {
   console.log("message info", ctx.message);
   const message = ctx.message;
-  if (
-    message.text.match(
-      /(\b(https?):\/\/)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g
-    )
-  ) {
-    ctx.reply("link yes!");
-    ogs({ url: message.text }).then((data) => {
-      const { error, result, response } = data;
-      console.log("error:", error); // This returns true or false. True if there was an error. The error itself is inside the results object.
-      console.log("result:", result); // This contains all of the Open Graph results
-      console.log("response:", response); // This contains the HTML of page
-    });
+  if (message.entities && message.entities.length > 0) {
+    if(message.entities[0].type === 'url') {
+      ogs({ url: message.text }).then((data) => {
+        const { error, result, response } = data;
+        console.log("error:", error); // This returns true or false. True if there was an error. The error itself is inside the results object.
+        console.log("result:", result); // This contains all of the Open Graph results
+        console.log("response:", response); // This contains the HTML of page
+        ctx.reply("link yes!");
+        return;
+      });
+    }
   }
   ctx.reply("蛤？dalao在说什么？");
 });
