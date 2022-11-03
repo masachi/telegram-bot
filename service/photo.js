7102f0875be74c959e94734ddaf87a0aada53ad1
masachi <zhangjb42@gmail.com>
2022-11-03 16:14:12 +0800

    fix: upload error

--- service/photo.js
+++ service/photo.js
@@ -5,18 +5,27 @@ const { v4: uuidv4 } = require('uuid');
 const processPhotoMessage = async (ctx, message) => {
   console.log("Photo Message", JSON.stringify(message));
   if (message.photo && message.photo.length > 0) {
+    let waitForUploadCount = 0;
     let uploadContent = [];
     let id = uuidv4();
     for(let photoItem of message.photo) {
       let uploadResponse = await uploadPhotoToTelegraph(ctx, photoItem);
-      uploadContent.push({
-        id: id,
-        ...photoItem,
-        ...uploadResponse
-      })
+      if(uploadResponse?.link) {
+        uploadContent.push({
+          id: id,
+          ...photoItem,
+          ...uploadResponse
+        });
+        waitForUploadCount++;
+      }
     }
-    await appendGistByGistId(process.env.GIST_ID, uploadContent, process.env.FILE_NAME);
-    return uploadContent[uploadContent.length - 1];
+    if(waitForUploadCount === message.photo.length) {
+      await appendGistByGistId(process.env.GIST_ID, uploadContent, process.env.FILE_NAME);
+      return uploadContent[uploadContent.length - 1];
+    }
+    else {
+      console.log("这条消息对应图片存在上传错误，请重新上传");
+    }
   }
 
   return {
