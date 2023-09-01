8a665d9d068f1f091b74761d0098547bdc9819ac
masachi <zhangjb42@gmail.com>
2023-09-01 12:30:22 +0800

    fix: json parse error

--- service/gist.js
+++ service/gist.js
@@ -41,7 +41,7 @@ getContentByGistId = async (gist_id, fileName) => {
     const gistGetResponse = await octokit.request(`GET /gists/${gist_id}`);
     console.log("获取gist 内容 Response 成功")
     if(gistGetResponse.status === 200) {
-        console.log("获取gist 内容 Done", gist_id)
+        console.log("获取gist 内容 Done", gist_id, gistGetResponse.data.files[fileName].raw_url)
         let rawJsonResponse = await fetch(gistGetResponse.data.files[fileName].raw_url)
         let rawJson = await rawJsonResponse.json()
         return rawJson
